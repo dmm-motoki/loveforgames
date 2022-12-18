1b51e2c54cd55d0f76111595a72d6594ae37ddc3
motoki3150 <108526062+motoki3150@users.noreply.github.com>
2022-12-18 05:31:39 +0000

    lovefo_games

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -7,6 +7,12 @@ class Admin::PostsController < ApplicationController
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
-    redirect_to admin_users_path
+    redirect_to request.referer
+  end
+
+  def destroy_in_postshow
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to admin_games_path
   end
 end
