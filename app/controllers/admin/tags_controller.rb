94fb65b8fa5f13a2e851ff3588adc7f64c0e9c88
motoki3150 <108526062+motoki3150@users.noreply.github.com>
2023-02-19 12:37:20 +0000

    loveforgames

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -1,11 +1,21 @@
 class Admin::TagsController < ApplicationController
   def index
     @tags = Tag.all
+    @tag = Tag.new
   end
 
   def show
     @tag = Tag.find(params[:id])
     @posts = @tag.posts.all
+  end
+
+  def create
+    @tag = Tag.new(tag_params)
+    if @tag.save
+      redirect_to admin_tags_path
+    else
+      render :index
+    end
   end
 
    private
